942e925e1f77fe5d06227deaffbe25ea49d8b981
fl0kie <fl0kie@mailbox.org>
2020-12-03 00:39:50 +0100

    bottom layer, some fancy stuff

---
+++ chaos_zone_shared.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="chaos_zone_shared" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
+ <image source="../tileSets/chaos_zone_shared.png" width="1024" height="2048"/>
+ <tile id="150">
+  <animation>
+   <frame tileid="101" duration="1000"/>
+   <frame tileid="102" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="214">
+  <animation>
+   <frame tileid="196" duration="1000"/>
+   <frame tileid="197" duration="1000"/>
+   <frame tileid="198" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="246">
+  <animation>
+   <frame tileid="230" duration="700"/>
+   <frame tileid="231" duration="700"/>
+  </animation>
+ </tile>
+</tileset>
