7e1ca28276c81309d54f55364ce833345b104a1a
fl0kie <>
2020-12-03 19:39:12 +0100

    deleted unused files

--- zw.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="zw" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
- <image source="../tileSets/zw.png" width="1024" height="2048"/>
-</tileset>
