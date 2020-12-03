36a7019935e8c071cd8053309d62bc0bd91b1b93
fl0kie <>
2020-12-03 19:30:45 +0100

    edited source paths

--- floortiles.tsx
+++ floortiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="floortiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../tileSets/floortiles.png" width="512" height="512"/>
+ <image source="tileSets/floortiles.png" width="512" height="512"/>
  <terraintypes>
   <terrain name="Stone" tile="19"/>
  </terraintypes>
