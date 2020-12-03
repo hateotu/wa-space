d60463f1473e003a0b5833ed7ab12664bd049f99
fl0kie <>
2020-12-03 19:15:40 +0100

    map update

--- floortiles.tsx
+++ floortiles.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="floortiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="../tileSets/floortiles.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="Stone" tile="19"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
 </tileset>
